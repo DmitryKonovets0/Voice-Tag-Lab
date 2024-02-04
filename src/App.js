2c8c6119d39145bb1a8f42b3cba3263a35f0a6b4
DmitryKonovets0 <dmitrykonovets@gmail.com>
2024-02-04 22:44:24 +0200

    implement fifth section - "Pricing" with animation for prices blocks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Component} from "react";
 import Promo from "./components/promo/promo";
 import How from "./components/how/how";
 import About from "./components/about/about";
+import Pricing from "./components/pricing/pricing";
 
 import './App.scss';
 import Samples from "./components/samples/samples";
@@ -15,6 +16,7 @@ class App extends Component{
                 <How/>
                 <About/>
                 <Samples/>
+                <Pricing/>
             </>
 
         )
---
+++ src/components/pricing/pricing.jsx
@@ -0,0 +1,87 @@
+import {Component} from "react";
+
+import './pricing.scss'
+
+
+export default class Pricing extends Component {
+
+    render() {
+        return(
+            <div className="pricing">
+                <div className="container">
+                    <div className="pricing__wrapper">
+                        <div className="first__col">
+                            <div className="pricing__title">Pricing</div>
+                            <div className="first__item"><span>1 Voice</span> <br/> You can pick any voice</div>
+                            <div className="first__item"><span>2 Voices</span> <br/> You can pick any 2 voices</div>
+                            <div className="first__item"><span>3 Voices</span> <br/> You can pick any 3 voices</div>
+                            <div className="first__item"><span>4 Voices</span> <br/> You can pick all 4 voices</div>
+                        </div>
+                        <div className="col">
+                            <div className="tag">1 Tag</div>
+                            <div className="item__price ">
+                                <div className="old-price">$31</div>
+                                <div className="new__price">$24.90</div>
+                            </div>
+                            <div className="clean__item"></div>
+                            <div className="clean__item"></div>
+                            <div className="clean__item"></div>
+                        </div>
+                        <div className="col">
+                            <div className="tag">2 Tag</div>
+                            <div className="item__price">
+                                <div className="old-price">$31</div>
+                                <div className="new__price">$39.90</div>
+                            </div>
+                            <div className="item__price">
+                                <div className="old-price">$31</div>
+                                <div className="new__price">$44.90</div>
+                            </div>
+                            <div className="clean__item"></div>
+                            <div className="clean__item"></div>
+                        </div>
+                        <div className="col">
+                            <div className="tag">3 Tag</div>
+                            <div className="item__price">
+                                <div className="old-price">$31</div>
+                                <div className="new__price">$49.90</div>
+                            </div>
+                            <div className="item__price">
+                                <div className="old-price">$31</div>
+                                <div className="new__price">$59.90</div>
+                            </div>
+                            <div className="item__price">
+                                <div className="old-price">$31</div>
+                                <div className="new__price">$64.90</div>
+                            </div>
+                            <div className="clean__item"></div>
+                        </div>
+                        <div className="col">
+                            <div className="tag">4 Tag</div>
+                            <div  className="item__price">
+                                <div className="old-price">$31</div>
+                                <div className="new__price">$59.90</div>
+                            </div>
+                            <div className="item__price">
+                                <div className="old-price">$31</div>
+                                <div className="new__price">$69.90</div>
+                            </div>
+                            <div className="item__price">
+                                <div className="old-price">$31</div>
+                                <div className="new__price">$74.90</div>
+                            </div>
+                            <div className="item__price">
+                                <div className="old-price">$31</div>
+                                <div className="new__price">$79.90</div>
+                            </div>
+                        </div>
+                        <button className="pricing__btn">
+                            Get your custom tag
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+}
