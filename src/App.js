74cee10e0441383e1eae49ec6f923165164fb435
DmitryKonovets0 <dmitrykonovets@gmail.com>
2024-02-11 17:49:28 +0200

    Implement responsive layout in ninth section

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component{
                 <About/>
                 <Samples/>
                 <Pricing/>
-                <Reviews/>
+                {/*<Reviews/>*/}
                 <FAQ/>
                 <Question/>
                 <Footer/>
