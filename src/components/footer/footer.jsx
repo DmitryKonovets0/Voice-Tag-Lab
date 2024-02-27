3f7c2f9c6024c04c144c2edf2ab2d37f29baebe2
Dmitry_Konovets <dmitry_konovets@Dmitry-Konovetss-Laptop.local>
2024-02-27 07:47:40 +0200

    Implement media styles from 425px to 768px in section -"footer"

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -10,19 +10,47 @@ import './footer.scss'
 import './footer-media.scss'
 
 export default class Footer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mobile: window.innerWidth < 1000
+        }
+    }
     render() {
+        const {mobile} = this.state
         return(
             <footer className="footer">
                 <div className="container">
                 <img src={logo} alt="logo" className="footer__logo"/>
-                <div className="footer__text-wrapper">
-                    <div className="footer__text-item">About us</div>
-                    <div className="footer__text-item">Tags Samples</div>
-                    <div className="footer__text-item">Pricing</div>
-                    <div className="footer__text-item">Reviews</div>
-                    <div className="footer__text-item">FAQ</div>
-                    <div className="footer__text-item">Contacts</div>
-                </div>
+                    <div className="footer__text-wrapper">
+                        {mobile ? (
+                            <div className="mobile">
+                                <div className="footer__text-item">About us</div>
+                                <div className="footer__text-item">Tags Samples</div>
+                                <div className="footer__text-item">Pricing</div>
+                            </div>
+                        ) : (
+                            <>
+                                <div className="footer__text-item">About us</div>
+                                <div className="footer__text-item">Tags Samples</div>
+                                <div className="footer__text-item">Pricing</div>
+                            </>
+                        )}
+                        {mobile ? (
+                            <div className="mobile">
+                                <div className="footer__text-item">Reviews</div>
+                                <div className="footer__text-item">FAQ</div>
+                                <div className="footer__text-item">Contacts</div>
+                            </div>
+                        ) : (
+                            <>
+                                <div className="footer__text-item">Reviews</div>
+                                <div className="footer__text-item">FAQ</div>
+                                <div className="footer__text-item">Contacts</div>
+                            </>
+                        )}
+
+                    </div>
                     <div className="footer__social-wrapper">
                         <img src={youtube} alt="" className="footer__social-item"/>
                         <img src={instargram} alt="" className="footer__social-item"/>
